42b199ca01009935fa515bc9e2e79746ce9ecd9e
whddus9509 <whddus9509@naver.com>
2020-07-18 17:48:08 +0900

    Initial commit

---
+++ src/test/java/com/jongyeon/practice_media/PracticeMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongyeon.practice_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
