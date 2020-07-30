8a0dc15959cf229cb55fef7e7f29f900563b7d18
whddus9509 <whddus9509@naver.com>
2020-07-30 23:16:33 +0900

    Create ExceptionHandler.java

---
+++ src/main/java/com/jongyeon/practice_media/common/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.jongyeon.practice_media.common;
+
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    private Logger log= LoggerFactory.getLogger(this.getClass());
+    @org.springframework.web.bind.annotation.ExceptionHandler({SizeLimitExceededException.class, MaxUploadSizeExceededException.class})
+    public void UploadFileSizeExceptionHandler(HttpServletRequest request, Exception ex){
+
+        log.error("ex!!!!!!!!!!!!!"+ex);
+
+    }
+}
